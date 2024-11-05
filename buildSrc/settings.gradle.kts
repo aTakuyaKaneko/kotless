18928d566baeedc8800b4de8ce4ea18324627898
Takuya Kaneko <Takuya.Kaneko@access-company.com>
2024-11-05 15:52:44 +0900

    Fix build error: Could not find org.codehaus.groovy.modules.http-builder:http-builder:0.7.2
    ----
    
    FAILURE: Build failed with an exception.
    
    * What went wrong:
    A problem occurred configuring project ':buildSrc'.
    > Could not resolve all artifacts for configuration ':buildSrc:classpath'.
       > Could not find org.codehaus.groovy.modules.http-builder:http-builder:0.7.2.
         Searched in the following locations:
           - https://plugins.gradle.org/m2/org/codehaus/groovy/modules/http-builder/http-builder/0.7.2/http-builder-0.7.2.pom
           - https://jcenter.bintray.com/org/codehaus/groovy/modules/http-builder/http-builder/0.7.2/http-builder-0.7.2.pom
           - https://releases.jfrog.io/artifactory/oss-releases/org/codehaus/groovy/modules/http-builder/http-builder/0.7.2/http-builder-0.7.2.pom
         Required by:
             project :buildSrc > tanvd.kosogor:tanvd.kosogor.gradle.plugin:1.0.9 > tanvd.kosogor:kosogor:1.0.9 > com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4
    
    * Try:
    Run with --stacktrace option to get the stack trace. Run with --info or --debug option to get more log output. Run with --scan to get full insights.
    
    * Get more help at https://help.gradle.org
    
    BUILD FAILED in 7s

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -3,5 +3,6 @@ pluginManagement {
         gradlePluginPortal()
         jcenter()
         maven(url = "https://releases.jfrog.io/artifactory/oss-releases/")
+        maven(url = "https://groovy.jfrog.io/artifactory/libs-release/")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
         jcenter()
         maven(url = "https://releases.jfrog.io/artifactory/oss-releases/")
+        maven(url = "https://groovy.jfrog.io/artifactory/libs-release/")
     }
 }
 
